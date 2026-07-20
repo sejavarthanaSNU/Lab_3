369b11525a4edb9964109ee8eadb53cf55d10968
kavitha1414 <kavitha.sampath1414@gmail.com>
2026-07-20 20:09:02 +0530

    Student B deletes registration module

--- Src/register.py
+++
@@ -1,8 +0,0 @@
-def register(username, password):
-    if username and password:
-        print("Registration successful")
-    else:
-        print("Username and password are required")
-
-
-register("user", "1234")
