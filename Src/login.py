7096c4f1ae99d25bcc799cbe62de007986a0a447
sejavarthana <sejavarthana.a@gmail.com>
2026-07-20 20:57:43 +0530

    Accidentally delete login module

--- Src/login.py
+++
@@ -1,8 +0,0 @@
-def login(username, password):
-    if username == "admin" and password == "1234":
-        print("Login successful")
-    else:
-        print("Invalid credentials")
-
-
-login("admin", "1234")
